8887d359ce0c20fbfca45c1cb47d1ee27cd61aea
Toushik Ahmed <pronay5271@gmail.com>
2024-07-13 15:21:41 +0600

    Added getLoggedInUser

--- routers/createuser.ts
+++ routers/createuser.ts
@@ -1,13 +1,15 @@
-import { Router } from 'express';
+import { Response, Router } from 'express';
 import { createUser, login } from '../controllers/usercreate';
 import { getAlluser } from '../controllers/allUserget';
 import { authMiddleware } from '../middlewares/auth';
+import { ExtendedRequest } from '../interfaces/extendedRequest';
 
 const router = Router();
 
 router.post('/signup', createUser);
 
 router.post('/login', login);
-router.get('/get',authMiddleware, getAlluser)
+
+router.get('/getLoggedInUser', authMiddleware, (req:ExtendedRequest, res: Response) => res.send(req.user))
 
 export default router;
