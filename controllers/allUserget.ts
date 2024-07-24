33d3df2072657ade2ce19a79f6c599d698b3dd0c
Toushik-Ahmed <Pronay5271@gmail.com>
2024-07-24 18:19:36 +0600

    after c

--- controllers/allUserget.ts
+++ controllers/allUserget.ts
@@ -1,13 +1,13 @@
-import { Request, Response } from 'express';
-import { findAlluser } from '../models/users table/getAlluser';
+import { Response } from 'express';
 import { ExtendedRequest } from '../interfaces/extendedRequest';
+import { findAlluser } from '../models/users table/getAlluser';
 
 export async function getAlluser(req: ExtendedRequest, res: Response) {
-console.log(req.user)
+  console.log(req.user);
   try {
     const allUsers = await findAlluser();
     res.send(allUsers);
   } catch (error) {
-    console.log('error occured');
+    console.log(error);
   }
 }
